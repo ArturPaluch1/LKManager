0003a3cfe168a796e7ec91990c3487569d8c78ed
Artur <artur_paluch@hotmail.com>
2023-08-17 19:50:46 +0200

    dodany timer aktywujący stronę

--- lk-manager-web/src/main/java/LKManager/LKManagerApplication.java
+++ lk-manager-web/src/main/java/LKManager/LKManagerApplication.java
@@ -9,31 +9,143 @@ import org.springframework.scheduling.annotation.EnableAsync;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URL;
+import java.net.URLConnection;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
+import java.util.Date;
+import java.util.Set;
+import java.util.Timer;
+import java.util.TimerTask;
 
 
 @SpringBootApplication
 public class LKManagerApplication {
 
+
+	private static long delay=14l;
+
+	protected static void startTimer()
+	{
+
+
+ LocalDateTime now;
+		now= LocalDateTime.now();
+
+		Timer timer = new Timer();
+
+		// long delay = 1000L;
+
+		TimerTask task = new TimerTask()
+		{
+			public void run() {
+				System.out.println("Task performed on: " + new Date() + "n" +
+						"Thread's name: " + Thread.currentThread().getName());
+
+				URL url = null;
+				try {
+					url = new URL("https://lkm-fgim.onrender.com/");
+			//		url = new URL("https://www.developer.com/java/java-event-listeners/");
+				} catch (MalformedURLException e) {
+					throw new RuntimeException(e);
+				}
+				URLConnection urlConnection = null;
+				try {
+					urlConnection = url.openConnection();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+				Object c= null;
+				try {
+					c = urlConnection.getContent();
+				} catch (IOException e) {
+					throw new RuntimeException(e);
+				}
+				System.out.println(c.getClass());
+
+
+
+
+
+
+
+
+
+
+
+
+
+				Set<Thread> threads = Thread.getAllStackTraces().keySet();
+				System.out.printf("%-15s \t %-15s \t %-15s \t %s\n", "Name", "State", "Priority", "isDaemon");
+				for (Thread t : threads) {
+					System.out.printf("%-15s \t %-15s \t %-15d \t %s\n", t.getName(), t.getState(), t.getPriority(), t.isDaemon());
+				}
+
+
+
+			}
+		};
+
+
+
+			timer.schedule(task,0,750000);
+		//timer.schedule(task, Date.from(now.plusMinutes(1).atZone(ZoneId.systemDefault()).toInstant()) );
+
+
+
+
+
+
+
+
+
+
+	}
+
+
 	public static void main(String[] args) {
 
 		SpringApplication.run(LKManagerApplication.class, args);
 
+startTimer();
+
+
+
+
+
+
+
+
 	}
+
+
 
 	@Bean
 	ApplicationRunner applicationRunner (Environment environment)
 	{
 		return  args -> {
 			System.out.println(environment.getProperty("msg"));
+
+
+
+
+
+
 		};
 	}
 	@PostConstruct
 	public void init() {
 		System.out.println("Initialized...");
+
 	}
 	@PreDestroy
 	public void destroy() {
