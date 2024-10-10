12663ec76aaeb871a7532b75760b4b17331cc892
Artur <artur_paluch@hotmail.com>
2024-10-10 02:50:54 +0200

    test 5 10.10.24 fixed email response server path (was hardcoded localhost) Added jars Removed secret key generators
    added lk-manager-web\target jar
    
    changed prop yaml
    
    changed dotenv for property in yaml

--- lk-manager-data/src/main/java/LKManager/DAO_SQL/UserDAO.java
+++ lk-manager-data/src/main/java/LKManager/DAO_SQL/UserDAO.java
@@ -11,12 +11,14 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.redis.core.RedisHash;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 import java.util.Optional;
 
 //@Repository
 @RedisHash
+@Repository
 public interface UserDAO extends JpaRepository<User, Long>, CustomUserDAO, CustomUserDAO_Redis  {
 
 
